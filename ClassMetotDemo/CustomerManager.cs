dc21fa42c76e845bf0e945c45c97ff1940da2941
hmyrbnl <hmyrbnl@gmail.com>
2021-02-19 21:30:15 +0300

    Add project files.

---
+++ ClassMetotDemo/CustomerManager.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class CustomerManager
+    {
+        public void Add(Customer customer)
+        {
+            Console.WriteLine(customer.Name + " " +customer.Surname + "kişisi eklendi");
+        }
+
+        public void Delete(Customer customer)
+        {
+            Console.WriteLine(customer.Name + " " + customer.Surname + "kişisi silindi");
+        }
+
+        public void Display(Customer customer)
+        {
+            Console.WriteLine(customer.Name + " " + customer.Surname + "kişisi listelendi");
+        }
+    }
+}
---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Customer customer1 = new Customer();
+            customer1.Name = "Ayse";
+            customer1.Surname = "Demir";
+            customer1.Phone = 12345;
+
+            Customer customer2 = new Customer();
+            customer2.Name = "Ali";
+            customer2.Surname = "Yıldız";
+            customer2.Phone = 46985;
+
+            Customer[] customers = new Customer[] { customer1, customer2 };
+
+            foreach (var customer in customers)
+            {
+                Console.WriteLine(customer.Name);
+                Console.WriteLine(customer.Surname);
+            }
+
+            CustomerManager customerManager = new CustomerManager();
+            customerManager.Add(customer1);
+            customerManager.Add(customer2);
+
+        }
+    }
+}
